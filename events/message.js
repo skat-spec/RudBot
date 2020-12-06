935ef36602d5f813b4eab4d6b2ec5e9e4bf37053
Xaliks <miskinmatvej77@gmail.com>
2020-12-06 22:37:31 +0300

    Все пофиксил (возможно)

--- events/message.js
+++ events/message.js
@@ -1,19 +1,17 @@
 const { 
     Xaliks, 
-    emojiId 
+    emojiId
 } = require('../config.json')
+const cfg = require('../config.json')
 const { 
     getServerPrefix,
     getUserRep,
     setUserRep, 
     setServerPrefix, 
-    setLevelUserRep, 
-    getLevelUserRep,
     getBlacklistUsers,
     error,
     timer
 } = require("../utils/functions");
-const bad = require('../data/bad.json')
 const now = Date.now();
 
 //Вадим не бей!) (украдено у Freedom)
@@ -33,22 +31,11 @@ const time = (time) => {
  module.exports = {
     name: "message",
     async execute(bot, message) {
-        //ТБР
-        if (message.content.startsWith('112') && message.guild.id === '681142809654591501' && !message.author.bot) return message.channel.send('<@&773267105243201546> <@&681200931764961306> <@&704666508869238835> <@&733340381890084925>!' + ` by ${message.author}`);
-        let i = 0
-        for (; i < 1000; i++) {
-          if(message.guild?.id != '681142809654591501' || message.channel.id === '681532459539890205' || message.channel.id === '780340363516575784') break
-            let args = message.content.toLowerCase().trim().split(/ +/);
-            if(bad.includes(args[i])) message.delete({ reason: 'Анти-мат'})
-        }
-
-        //Команды
-        const prefix = (await getServerPrefix(message.guild.id)) || "/";
+        const prefix = (await getServerPrefix(message.guild.id)) || cfg.prefix;
         const serverPrefix = await getServerPrefix(message.guild.id)
         const blacklistedUsers = await getBlacklistUsers();
         const userId = message.author.id;
         const userRep = await getUserRep(message.guild.id, userId);
-        const userRepLevel = await getLevelUserRep(message.guild.id, userId)
         const args = message.content.slice(prefix.length).trim().split(/ +/g);
         const cooldowns = bot.cooldowns;
         const commandName = args.shift().toLowerCase();
@@ -62,14 +49,12 @@ const time = (time) => {
             || message.content === prefix
             || message.author.bot
             || userId === bot.user.id
-            || (command.category === 'tbr' && message.guild.id != '681142809654591501')
         ) return;
         if (bot.commands.has(command?.name)) {
         const timestamps = cooldowns.get(command.name);
         const cooldownAmount = (command.cooldown || 3) * 1000;
         if (userRep === null || !userRep) setUserRep(message.guild.id, userId, 0);
-        if (serverPrefix === null || !serverPrefix) setServerPrefix(message.guild.id, '/');
-        if(userRepLevel === null || !userRepLevel) setLevelUserRep(message.guild.id, userId, 'Кто-то');
+        if (serverPrefix === null || !serverPrefix) setServerPrefix(message.guild.id, cfg.prefix);
         if (message.content === `<@!${bot.user.id}>`) return message.channel.send(`Префикс на этом сервере: \`${getServerPrefix(message.guild.id)}\``);
         if (command.args && !args.length) {
             let err = 'Недостаточно аргументов!'
@@ -82,7 +67,7 @@ const time = (time) => {
         return}
         if (timestamps.has(message.author.id)) {
             const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
-            if (now < expirationTime && message.author.id != '448799481777881089') {
+            if (now < expirationTime && message.author.id != Xaliks) {
                 const timeLeft = time(expirationTime);
                 return message.channel.send(error(`Пожалуйста, подожди еще \`${timeLeft}\` прежде чем использовать команду \`${command.name}\``));
             }
@@ -94,11 +79,7 @@ const time = (time) => {
         //ЧС
         if (blacklistedUsers !== null) {
             let isBlacklisted = blacklistedUsers.filter(u => u.user?.id === message.author.id)[0];
-            if (isBlacklisted && !message.content.startsWith('/идея')) return message.react(emojiId.error)
-            else if(message.content.startsWith('/идея') && isBlacklisted) {
-                message.react(emojiId.error)
-                message.channel.send(error('Вам запрещено писать идеи!'))
-            return}
+            if (isBlacklisted) return message.react(emojiId.error)
         }
     
         try {
--- utils/functions.js
+++ utils/functions.js
@@ -131,7 +131,6 @@ module.exports = {
   setUserRep,
   addUserRep,
   getUserRep,
-  remUserRep,
   timer,
   time
 };
