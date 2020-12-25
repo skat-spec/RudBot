4371dcf60cbd580c7c9eba6ef3663754bb1ad13c
Xaliks <miskinmatvej77@gmail.com>
2020-12-25 22:37:26 +0300

    Фикс: null если у чела только эмодзи

--- commands/info/user-info.js
+++ commands/info/user-info.js
@@ -23,24 +23,26 @@ module.exports = {
     category: 'info',
     async execute(message, args, bot) {
         const member = findMember(message, args.join(' '), true)
-        const {
-            id
-        } = member;
-        const data = [];
-        if(member.presence.status === 'offline') data.push(`${emoji.offline} Оффлайн`)
+
+        //Статус
+        const clientStatus = [];
+        if(member.presence.status === 'offline') clientStatus.push(`${emoji.offline} Оффлайн\n`)
         else {
-            if(member.presence.clientStatus.web) data.push(`${emoji[member.presence.clientStatus.web]} Сайт`);
-            if(member.presence.clientStatus.mobile) data.push(`${emoji[member.presence.clientStatus.mobile]} Телефон`);
-            if(member.presence.clientStatus.desktop) data.push(`${emoji[member.presence.clientStatus.desktop]} Компьютер`);
+            if(member.presence.clientStatus.web) clientStatus.push(`${emoji[member.presence.clientStatus.web]} Сайт\n`);
+            if(member.presence.clientStatus.mobile) clientStatus.push(`${emoji[member.presence.clientStatus.mobile]} Телефон\n`);
+            if(member.presence.clientStatus.desktop) clientStatus.push(`${emoji[member.presence.clientStatus.desktop]} Компьютер\n`);
         }
+
+        //Создан / Зашел
         let date1 = new Date(message.createdTimestamp)
         let date2 = new Date(member.user.createdTimestamp)
         let date3 = new Date(message.guild.member(member).joinedTimestamp)
         const createdAtMS = Math.round(Math.abs((date1.getTime() - date2.getTime())))
         const joinedAtMS = Math.round(Math.abs((date1.getTime() - date3.getTime())))
-        let ubadges = 'Остсутствуют'
         const joinedAt = formatDate(date3)
         const createdAt = formatDate(date2)
+
+        //Роли
         const roles =
             member.roles.cache
             .filter((r) => r !== message.guild.id)
@@ -49,40 +51,44 @@ module.exports = {
             .slice(0, -1)
             .join(", ") || "**Остсутствуют**"
 
-        const roleCount = member.roles.cache.filter(
-            (r) => r.id !== message.guild.id
-        ).size;
-        let status;
-        if(member.user.presence.status != 'offline') {
-            if(member.user.presence.activities[0]) {
-                if(member.user.presence.activities[0].type === 'CUSTOM_STATUS') status = `\n ${member.user.presence.activities[0].state} \n`;
-                else status = `\n${ActivityType[member.user.presence.activities[0].type]} **${member.user.presence.activities[0].name}**`
-            }
+        const Roles = member.roles.cache.size - 1
+
+        //Статус
+        let status = '';
+        let activity = '\n';
+        if(member.user.presence.status != 'offline' && member.user.presence.activities[0]) {
+            if(member.user.presence.activities[0].type === 'CUSTOM_STATUS') status = `${member.user.presence.activities[0].state === null ? '' : member.user.presence.activities[0].state}`;
+            else status += `${ActivityType[member.user.presence.activities[0].type]} **${member.user.presence.activities[0].name}**`
+
             let i = 1
             for(; i < 11; i++) {
                 if(member.user.presence.activities[i]) {
-                    if(member.user.presence.activities[i].type === 'PLAYING' || 'STREAMING' || 'LISTENING' || 'WATCHING') status += `\n${ActivityType[member.user.presence.activities[i].type]} **${member.user.presence.activities[i].name}**`
+                    if(member.user.presence.activities[i].type === 'PLAYING' || 'STREAMING' || 'LISTENING' || 'WATCHING') activity += `${ActivityType[member.user.presence.activities[i].type]} **${member.user.presence.activities[i].name}**\n`
                 }
             }
         }
-        if(member.user.flags.toArray() != '') ubadges = `${member.user.flags.toArray().map(flag => badges[flag]).join(', ')}`
 
+        //Значки
+        let ubadges = 'Остсутствуют'
+        if(member.user.flags?.toArray() != '') ubadges = `${member.user.flags.toArray().map(flag => badges[flag]).join(', ')}`
+
+        //Эмбед
         message.channel.send(new MessageEmbed()
             .setTitle(`Информация о пользователе`)
             .setAuthor(member.user.tag, member.displayAvatarURL)
             .setDescription(`Ник: **${member.user.tag} || ${member.user}**
-ID: **${id}**
-Статус: ${data.join(',\n')}
-${status}
-Значки: **${ubadges}**
+ID: **${member.id}**
+Статус: ${clientStatus}${status}
+${activity}Значки: **${ubadges}**
 
-**Роли(${roleCount}):** ${roles}
+**Роли(${Roles}):** ${roles}
 
 Аккаунт создан: **${createdAt}** (${getDay(createdAtMS)} назад)
 Зашел на сервер: **${joinedAt}** (${getDay(joinedAtMS)} назад)`)
             .setColor(member.displayHexColor || '303136')
             .setThumbnail(member.user.displayAvatarURL({
-                dynamic: true
+                dynamic: true,
+                size: 2048
             }))
             .setFooter('Дизайн JeggyBot')
             .setTimestamp())
